e950516e7ee9dfa7083a50ef842ce8ea54b0dd38
bluexu <xhm520g@gamil.com>
2016-04-05 11:47:05 +0800

    one

---
+++ expand-dottedLine.js
@@ -0,0 +1,28 @@
+/**
+ * Created by bluexu on 16/4/5.
+ */
+function expandDottedLine(id) {
+    var context = document.getElementById(id).getContext('2d'),
+        moveToFunction = CanvasRenderingContext2D.prototype.moveTo;
+    CanvasRenderingContext2D.prototype.lastMoveToLocation = {};
+
+    CanvasRenderingContext2D.prototype.moveTo = function (x, y) {
+        moveToFunction.apply(context, [x, y]);
+        this.lastMoveToLocation.x = x;
+        this.lastMoveToLocation.y = y;
+    };
+    CanvasRenderingContext2D.prototype.dashedLineTo = function (x, y, dashLenth) {
+        dashLenth = dashLenth === undefined ? 5 : dashLenth;
+        var startX = this.lastMoveToLocation.x;
+        var startY = this.lastMoveToLocation.y;
+
+        var deltaX = x - startX;
+        var deltaY = y - startY;
+        var numDashes = Math.floor(Math.sqrt(deltaX * deltaX + deltaY * deltaY) / dashLenth);
+        
+        for (var i =0;i<numDashes;++i){
+            
+        }
+    }
+
+};
